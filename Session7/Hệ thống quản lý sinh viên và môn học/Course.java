22896de50cab2c2e330a704f7557290993c5f5f4
Lê Hoàng Việt <leviet20051509@gmail.com>
2026-03-16 20:35:02 +0700

    Add files via upload

---
+++ Session7/Hệ thống quản lý sinh viên và môn học/Course.java
@@ -0,0 +1,9 @@
+package BaiTap.HeThongQuanLySinhVienVaMonHoc;
+
+public class Course {
+    private String code;
+    private String title;
+    public void showCourse(){
+
+    }
+}
---
+++ Session7/Hệ thống quản lý sinh viên và môn học/Student.java
@@ -0,0 +1,10 @@
+package BaiTap.HeThongQuanLySinhVienVaMonHoc;
+
+public class Student {
+    private int id;
+    private String name;
+    private Course course;
+    public void díplayInfo(){
+        
+    }
+}
