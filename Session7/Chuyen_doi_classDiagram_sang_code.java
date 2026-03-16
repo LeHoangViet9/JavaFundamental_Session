e53285c27621f147bbabd2753f1cbdb81ca85c56
Lê Hoàng Việt <leviet20051509@gmail.com>
2026-03-16 20:33:32 +0700

    Add files via upload

---
+++ Session7/Chuyen_doi_classDiagram_sang_code.java
@@ -0,0 +1,13 @@
+package BaiTap;
+
+public class Chuyen_doi_classDiagram_sang_code {
+    private String title;
+    private String author;
+    private double price;
+
+    public void printInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+}
