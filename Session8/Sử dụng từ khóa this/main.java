016dd4b2b9f5812e4259244b702e36fbfa823c6f
Lê Hoàng Việt <leviet20051509@gmail.com>
2026-03-18 21:10:57 +0700

    Add files via upload

---
+++ Session8/Sử dụng từ khóa this/main.java
@@ -0,0 +1,10 @@
+package SuDungTuKhoaThis;
+
+public class main {
+    public static void main(String[] args) {
+        Book b1=new Book("Doraemon","Fujiko",20000D);
+        Book b2=new Book("Tao dau an cua ban","Glei",990000D);
+        b1.display();
+        b2.display();
+    }
+}
